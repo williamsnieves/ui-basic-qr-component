61a64c634a26ecbb0948fd7cfc51ee294d234ef6
Willians Nieves <williansnieves@gmail.com>
2025-05-15 19:29:43 +0200

    chore: updated styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,14 @@ import QRDescription from "./components/QRDescription";
 
 function App() {
   return (
-    <main className="flex justify-center items-center h-screen font-outfit bg-slate-300">
-      <div className="bg-white rounded-lg shadow-lg p-4 pb-10 flex flex-col items-center w-[320px] max-w-full gap-6 max-h-[499px]">
+    <main className="flex justify-center items-center h-screen font-outfit bg-[#D5E1EF]">
+      <div className="bg-white rounded-lg shadow-lg p-4 pb-10 flex flex-col items-center w-[320px] max-w-full  max-h-[499px]">
         <Image
           url={`${import.meta.env.BASE_URL}image-qr-code.png`}
           alt="QR image"
         />
-        <main>
-          <QRDescription />
-        </main>
+
+        <QRDescription />
       </div>
     </main>
   );
--- src/components/QRDescription.tsx
+++ src/components/QRDescription.tsx
@@ -1,13 +1,13 @@
 function QRDescription() {
   return (
-    <section aria-labelledby="qr-title">
+    <section aria-labelledby="qr-title" className="mt-6">
       <h1
         id="qr-title"
-        className="text-2xl text-slate-900 font-bold text-center text-shadow-[0_4px_4px_rgb(0_0_0_/_0.25)]"
+        className="font-outfit font-bold text-[22px] leading-[1.2] text-center text-slate-900 text-shadow-[0_4px_4px_rgb(0_0_0_/_0.25)]"
       >
         Improve your front-end skills by building projects
       </h1>
-      <p className="text-slate-500 tracking-widest mt-4 text-[15px] leading-5 text-center mb-4">
+      <p className="text-[15px] leading-[1.4] tracking-[2px] text-center text-slate-500 mt-4">
         Scan the QR code to visit Frontend Mentor and take your coding skills to
         the next level
       </p>
