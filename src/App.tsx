90854f9eb4f9b5b1638e1402bd3389d33046646d
Willians Nieves <williansnieves@gmail.com>
2025-05-15 19:03:01 +0200

    refactor: polished styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import QRDescription from "./components/QRDescription";
 function App() {
   return (
     <main className="flex justify-center items-center h-screen font-outfit bg-slate-300">
-      <div className="bg-white rounded-lg shadow-lg p-4 pb-10 flex flex-col items-center w-[320px] max-w-full gap-6">
+      <div className="bg-white rounded-lg shadow-lg p-4 pb-10 flex flex-col items-center w-[320px] max-w-full gap-6 max-h-[499px]">
         <Image
           url={`${import.meta.env.BASE_URL}image-qr-code.png`}
           alt="QR image"
